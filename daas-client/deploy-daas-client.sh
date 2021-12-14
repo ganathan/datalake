2a5d6e7c65ce25b2de02df8229e63e293df68cb5
Ganesh-at-944071232105 <gnathan-at-944071232105>
2021-12-14 10:49:29 -0800

    initial deployment

--- daas-client/deploy-daas-client.sh
+++ daas-client/deploy-daas-client.sh
@@ -6,20 +6,21 @@ region=$3
 environment=$4
 
 # Check if parameters are defined
-if [ ! -z "$entity" ] && [ ! -z "$accountId" ] && [ ! -z "$application" ] && [ ! -z "$environment" ] && [ ! -z "$region" ] && [ ! -z "$serviceType" ] 
+if [ ! -z "$entity" ] && [ ! -z "$accountId" ] && [ ! -z "$environment" ] && [ ! -z "$region" ] 
 then
     # Deploy the VPC
-    sh ../deploy-stack.sh $entity $accountId daas-client $environment $region vpc
-    sleep 90
+    # sh ../daas-common/deploy-template.sh $entity $accountId $environment $region vpc
+    # sh ../deploy-stack.sh $entity $accountId daas-client $environment $region vpc
+    # sleep 90
     # Deploy the Nat Gateway
-    sh ../daas-common/deploy-template.sh $entity $accountId $environment $region ngw
-    sh ../deploy-stack.sh $entity $accountId daas-client $environment $region ngw
-    sleep 60
+    # sh ../daas-common/deploy-template.sh $entity $accountId $environment $region ngw
+    # sh ../deploy-stack.sh $entity $accountId daas-client $environment $region ngw
+    #sleep 60
     # Deploy the buckets
-    sh ../daas-common/deploy-template.sh $entity $accountId $environment $region s3
-    sh ../deploy-stack.sh $entity $accountId daas-client-athena-log $environment $region s3
-    sh ../daas-common/deploy-template.sh $entity $accountId $environment $region s3
-    sh ../deploy-stack.sh $entity $accountId daas-client-raw-bucket $environment $region s3
+    # sh ../daas-common/deploy-template.sh $entity $accountId $environment $region s3
+    # sh ../deploy-stack.sh $entity $accountId daas-client-athena-log $environment $region s3
+    # sh ../daas-common/deploy-template.sh $entity $accountId $environment $region s3
+    # sh ../deploy-stack.sh $entity $accountId daas-client-test-raw-bucket $environment $region s3
     # Deploy the lambda
     sh ../daas-common/deploy-template.sh $entity $accountId $environment $region lmd
     sh ../deploy-stack.sh $entity $accountId metadata-generator $environment $region lmd 1
