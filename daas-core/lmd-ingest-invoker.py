94f6d742711fcc711b1b2fda49c6eae73d7b9301
Ganesh Nathan <nathan.ganesh@gmail.com>
2022-05-26 09:13:07 -0700

    adding lf grants

--- daas-core/lmd-ingest-invoker.py
+++ daas-core/lmd-ingest-invoker.py
@@ -3,7 +3,6 @@ import json
 import os
 import urllib.parse
 import boto3
-import hashlib
 from io import StringIO
 
 
@@ -13,7 +12,7 @@ from io import StringIO
 def init():
     global s3, s3_client, stpfn_client, lambda_client, event_client, glue_client, glue_db_name 
     global lakeformation_role_name, target_lambda_name, daas_config, accountid, environment
-    global region, event_converter_stepfn_arn
+    global region, event_converter_stepfn_arn, event_controller_stepfn_arn
     stpfn_client = boto3.client('stepfunctions')
     s3_client = boto3.client('s3')
     s3 = boto3.resource('s3')
@@ -28,7 +27,39 @@ def init():
     lakeformation_role_name = os.environ["ENV_VAR_GLUE_SERVICE_ROLE"]
     target_lambda_name = os.environ["ENV_VAR_CLIENT_LAMBDA_NAME"]
     event_converter_stepfn_arn = os.environ["ENV_VAR_EVNT_CONVR_STEP_FUNC_ARN"]
+    event_controller_stepfn_arn = os.environ["ENV_VAR_EVNT_CONTROL_STEP_FUNC_ARN"]
 
+ 
+# -------------------------------------------------
+# Create the cloudwatch event for the crawler
+# -------------------------------------------------
+def create_cloudwatch_event(crawler_name):
+    rule_name = crawler_name + "-event"
+    event_json_string = json.dumps({'source': ['aws.glue'], 'detail-type': ['Glue Crawler State Change'],
+                                    'detail': {'crawlerName': [crawler_name], 'state': ['Succeeded']}})
+ 
+    # Create the rule first
+    rule_response = event_client.put_rule(
+        Name=rule_name,
+        EventPattern=event_json_string,
+        State='ENABLED',
+        Description='Cloud Watch event rule for crawler ' + crawler_name
+    )
+ 
+    # Place the lambda target for the rule
+    response = event_client.put_targets(
+        Rule=rule_name,
+        Targets=[{'Id': rule_name, 'Arn': target_lambda_arn}, ]
+    )
+ 
+    # Grant invoke permission to Lambda
+    lambda_client.add_permission(
+        FunctionName=target_lambda_name,
+        StatementId=rule_name,
+        Action='lambda:InvokeFunction',
+        Principal='events.amazonaws.com',
+        SourceArn=rule_response['RuleArn'],
+    )
 
 # ----------------------------------------------------------
 # Read the account id from the daas-config file
@@ -51,6 +82,11 @@ def get_client_entity_name(data_dict):
     entity_name = json.loads(data_dict)['entity']
     return entity_name
 
+def get_client_glue_database_name(data_dict):   
+    #glue_db_name = data_dict['glue_database']
+    glue_db_name = 'ganesh-gludb-raw-poc'
+    return glue_db_name
+
 # -------------------------------------------------------------------------------------------
 # Read the replication status, database name and database schema  from the daas-config file
 # -------------------------------------------------------------------------------------------
@@ -63,11 +99,11 @@ def get_replication_detail(data_dict):
         db_schema = json.loads(data_dict)['repl_db_info']['db_schema']
     return replicate, db_name, db_schema 
 
+
 # ----------------------------------------------------------
 # Invoke metadata generator lambda on the client account
 # ----------------------------------------------------------
-def invoke_lambda(target_lambda_arn, target_gluejb_lambda_arn, source_bucket, source_name, target_lambda_role_arn, crawler_name, 
-                path, domain_name, table_name, value, partition_values, replicate, db_name, db_schema):
+def invoke_lambda(target_lambda_arn, target_gluejb_lambda_arn, source_bucket, source_name, target_lambda_role_arn, crawler_name, path, domain_name, table_name, value, partition_values, replicate, db_name, db_schema):
     sts_connection = boto3.client('sts')
     daas_client = sts_connection.assume_role(
         RoleArn=target_lambda_role_arn,
@@ -82,7 +118,9 @@ def invoke_lambda(target_lambda_arn, target_gluejb_lambda_arn, source_bucket, so
     data['lakeformation_role_name'] = lakeformation_role_name
     data['gluejb_lambda_arn'] = target_gluejb_lambda_arn
     data['src_bucket_name'] = source_bucket
-    data['src_source_name'] = source_name
+    data['src_source_name'] = source_name    
+    # data['target_lambda_name'] = target_lambda_name
+    # data['target_lambda_arn'] = target_lambda_arn
     data['source_file_path'] = path
     data['domain_name'] = domain_name
     data['crawler_name'] = crawler_name
@@ -91,7 +129,7 @@ def invoke_lambda(target_lambda_arn, target_gluejb_lambda_arn, source_bucket, so
     data['partition_values'] = partition_values
     data['replicate'] = replicate
     data['db_name'] = db_name
-    data['db_schema'] = db_schema
+    data['db_schema'] = db_schema    
     json_str=json.dumps(data)
     response = lambda_client.invoke(
         FunctionName= target_lambda_arn, 
@@ -100,24 +138,60 @@ def invoke_lambda(target_lambda_arn, target_gluejb_lambda_arn, source_bucket, so
     )
     return response['Payload'].read()
 
-# ----------------------------------------------------------
-# Invoke step function to convert the object
-# ----------------------------------------------------------
-def invoke_stepfunction(source_bucket, source_key, domain_name, object_name, extension):
-    params = {
-        'source_bucket': source_bucket,
-        'source_key': source_key,
-        'domain_name': domain_name,
-        'object_name': object_name,
-        'extension': extension
-    }
+# -------------------------------------------------------------------
+# Invoke step function to contol the services deployed on the object
+# --------------------------------------------------------------------
+def invoke_controller_stepfunction(account_id, glue_db_name, commands, controller, state_machine_arn):
+    try:
+        params = {
+            'account_id': account_id,
+            'database_name': glue_db_name,
+            'commands': commands,
+            'controller': controller,
+            'state_machine_arn': state_machine_arn
+        }
 
-    response = stpfn_client.start_execution(
-        stateMachineArn=event_converter_stepfn_arn,
-        input= json.dumps(params)
-    )
-    return response['Payload'].read()
+        response = stpfn_client.start_execution(
+            stateMachineArn=state_machine_arn,
+            input= json.dumps(params)
+        )
+        return response['Payload'].read()
+    except Exception as e:
+        raise ValueError(f"Failed while invoking step function {state_machine_arn}  {params} {e}")
 
+# ---------------------------------------------------------------------
+# Invoke step function to converter the object
+# ---------------------------------------------------------------------
+def invoke_converter_stepfunction(source_bucket, source_key, domain_name, object_name, extension, state_machine_arn):
+    try:
+        params = {
+            'source_bucket': source_bucket,
+            'source_key': source_key,
+            'domain_name': domain_name,
+            'object_name': object_name,
+            'extension': extension
+        }
+
+        response = stpfn_client.start_execution(
+            stateMachineArn=state_machine_arn,
+            input= json.dumps(params)
+        )
+        return response['Payload'].read()
+    except Exception as e:
+        raise ValueError(f"Failed while invoking step function {state_machine_arn}  {params} {e}")
+
+# ---------------------------------------------------------------------
+# IGet config details from the client s3 bucket
+# ---------------------------------------------------------------------
+def get_config_details(source_bucket):
+    fileObj = s3_client.get_object(Bucket= source_bucket, Key=daas_config)
+    data_dict = fileObj['Body'].read()
+    account_id = get_client_accountid(data_dict)
+    region = get_client_region(data_dict)
+    entity = get_client_entity_name(data_dict)
+    (replicate, db_name, db_schema) = get_replication_detail(data_dict) 
+    database_name = get_client_glue_database_name(data_dict)
+    return (account_id, region, entity, replicate, db_name, db_schema, glue_db_name)
 # -------------------------------------------------
 # Main lambda function
 # -------------------------------------------------
@@ -134,10 +208,17 @@ def lambda_handler(event, context):
                 short_path = source_key[0:source_key.rfind('/',0)] # get upto the file path
                 is_dot_folder_object = short_path.rfind('.',0)
                 is_ignore_object = short_path.find('-daas-gen-raw')
-                if object_name and is_dot_folder_object == -1 and is_ignore_object == -1:
+                is_access_control = object_name.find('access-config.txt')              
+                if is_dot_folder_object != -1 and is_access_control != -1:
+                    controller = 'access-control'
+                    (account_id, region, entity, replicate, db_name, db_schema, glue_db_name) = get_config_details(source_bucket)
+                    fileObj = s3_client.get_object(Bucket= source_bucket, Key=source_key)
+                    commands = fileObj['Body'].read().decode('utf-8').splitlines()
+                    resp = invoke_controller_stepfunction(account_id, glue_db_name, commands, controller, state_machine_arn=event_controller_stepfn_arn)
+                elif object_name and is_dot_folder_object == -1 and is_ignore_object == -1:
                     extension = object_name.split('.')[-1].lower() # get the file extension.
-                    if extension == 'xml' or extension == 'xls' or extension == 'xlsx':
-                        invoke_stepfunction(source_bucket, source_key, domain_name, object_name, extension)
+                    if extension == 'xml' or extension == 'xls' or extension == 'xlsx' or extension == 'md':
+                        resp = invoke_converter_stepfunction(source_bucket, source_key, domain_name, object_name, extension, state_machine_arn=event_converter_stepfn_arn)
                     else:                   
                         partition_values=[]
                         source_name = source_key.split('/')[0:1][0]  # get the first prefix and assign it as the source name
@@ -150,12 +231,7 @@ def lambda_handler(event, context):
                             else:
                                 partition_values.append(key)
                         path = 's3://' + source_bucket + '/' + source_name + '/' +  domain_name + '/'
-                        fileObj = s3_client.get_object(Bucket= source_bucket, Key=daas_config)
-                        data_dict = fileObj['Body'].read()
-                        account_id = get_client_accountid(data_dict)
-                        region = get_client_region(data_dict)
-                        entity = get_client_entity_name(data_dict)
-                        (replicate, db_name, db_schema) = get_replication_detail(data_dict)
+                        (account_id, region, entity, replicate, db_name, db_schema) = get_config_details(source_bucket)
                         target_lambda_arn = 'arn:aws:lambda:' + region + ':' + account_id + ':function:' + target_lambda_name
                         target_lambda_role_arn = 'arn:aws:iam::' + account_id + ':role/rle-' + target_lambda_name
                         crawler_name = entity + '-' + source_name + '-' + domain_name + '-' + 'raw-crawler'
@@ -172,8 +248,7 @@ def lambda_handler(event, context):
                 'statusCode': 200
             }
         except Exception as e:
-            print(e)
             return {
-                'body': json.dumps(e),
+                'body': json.loads(json.dumps(e, default=str)),
                 'statusCode': 400
             }
