89b60412a57789258480539516478aee3abed7aa
Ganesh Nathan <nathan.ganesh@gmail.com>
2022-05-27 14:23:18 -0700

    updating metadata generator

--- daas-core/lmd-metadata-generator.py
+++ daas-core/lmd-metadata-generator.py
@@ -1,27 +1,18 @@
+# --------------------------------------------------------------------------
+# Author:       Ganesh Nathan
+# Date:         05/08/2020
+# Description:  DaaS common lambda to create distribution glue crawler, catalog and table
+# --------------------------------------------------------------------------
+import copy
 import json
 import boto3
 import os
-import copy
-
-
-# --------------------------------------------------
-# Initializes global variables
-# --------------------------------------------------
-def init():
-    global lambda_client, event_client, glue_client, entity, environment
-    glue_client = boto3.client('glue')
-    lambda_client = boto3.client('lambda')
-    event_client = boto3.client('events')
-    entity = os.environ["ENV_VAR_ENTITY"]
-    environment = os.environ['ENV_VAR_ENVIRONMENT']
-    account_id = os.environ['ENV_VAR_ACCOUNT_ID']
-    region = os.environ['ENV_VAR_REGION']
-
+    
 
 # -------------------------------------------------
 # Check if Crawler exits
 # -------------------------------------------------
-def crawler_exits(crawler_name):
+def crawler_exits(glue_client, crawler_name):
     try:
         # call the get crawler, if response fails then crawler is not defined
         response = glue_client.get_crawler(
@@ -35,7 +26,7 @@ def crawler_exits(crawler_name):
 # -------------------------------------------------
 # Check existence of partition in the Glue Table
 # -------------------------------------------------
-def partition_exists(glue_db_name, table_name, partition_values):
+def partition_exists(glue_client, glue_db_name, table_name, partition_values):
     try:
         response = glue_client.get_partition(
             DatabaseName=glue_db_name,
@@ -50,9 +41,9 @@ def partition_exists(glue_db_name, table_name, partition_values):
 # -------------------------------------------------
 # Add Partitions to Glue Table
 # -------------------------------------------------
-def add_table_partitions(glue_db_name, table_name, partitions, partition_values):
+def add_table_partitions(glue_client, glue_db_name, table_name, partitions, partition_values):
     # Check if partition exists already
-    if not partition_exists(glue_db_name, table_name, partition_values):
+    if not partition_exists(glue_client, glue_db_name, table_name, partition_values):
         get_table_response = glue_client.get_table(
             DatabaseName=glue_db_name,
             Name=table_name
@@ -61,7 +52,13 @@ def add_table_partitions(glue_db_name, table_name, partitions, partition_values)
         # Extract the existing storage descriptor and Create custom storage descriptor with new partition location
         storage_descriptor = get_table_response['Table']['StorageDescriptor']
         custom_storage_descriptor = copy.deepcopy(storage_descriptor)
-        custom_storage_descriptor['Location'] = storage_descriptor['Location'] + partitions[0] + '/'
+        custom_storage_descriptor['Location'] = storage_descriptor['Location'] + partitions + '/'
+
+        print('inside add table partition ' + table_name)
+        print(custom_storage_descriptor)
+        print(partitions)
+        print(partition_values)
+        print(glue_db_name)
 
          # Create partitions in the glue table
         response = glue_client.create_partition(
@@ -72,6 +69,7 @@ def add_table_partitions(glue_db_name, table_name, partitions, partition_values)
                 'StorageDescriptor': custom_storage_descriptor
             }
         )
+        print(response)
     else:
         print("partition exists...ignoring changes!")
 
@@ -79,13 +77,13 @@ def add_table_partitions(glue_db_name, table_name, partitions, partition_values)
 # -------------------------------------------------
 # Create new glue crawler
 # -------------------------------------------------
-def create_crawler(glue_db_name, lakeformation_role_name, crawler_name, domain_name, source_file_path):
+def create_crawler(glue_client, glue_db_name, glue_admin_role_name, crawler_name, domain_name, source_file_path):
     try:
         # call the get crawler, if response fails then crawler is not defined
         response = glue_client.create_crawler(
             Name=crawler_name,
             DatabaseName=glue_db_name,
-            Role=lakeformation_role_name,
+            Role=glue_admin_role_name,
             Description='Crawler to create catalog on ' + domain_name + ' domain. DaaS Generated!',
             Targets={
                 'S3Targets': [
@@ -104,12 +102,12 @@ def create_crawler(glue_db_name, lakeformation_role_name, crawler_name, domain_n
         )
         return response
     except Exception as e:
-        return e
+        print(e)
 
 # -------------------------------------------------
-# Start the new glue crawler
+# Create the new glue crawler
 # -------------------------------------------------
-def start_crawler(crawler_name):
+def start_crawler(glue_client, crawler_name):
     response = glue_client.start_crawler(
         Name=crawler_name
     )
@@ -118,19 +116,12 @@ def start_crawler(crawler_name):
 # -------------------------------------------------
 # Create the cloudwatch event for the crawler
 # -------------------------------------------------
-def create_cloudwatch_event(glue_db_name, gluejb_lambda_arn, src_bucket_name, src_source_name, domain_name, table_name, db_name, db_schema, crawler_name):
-    rule_name = entity + '-evt-' + crawler_name
+def create_cloudwatch_event(crawler_name, target_lambda_arn, target_lambda_name):
+    lambda_client = boto3.client('lambda')
+    event_client = boto3.client('events')
+    rule_name = crawler_name + "-event"
     event_json_string = json.dumps({'source': ['aws.glue'], 'detail-type': ['Glue Crawler State Change'],
                                     'detail': {'crawlerName': [crawler_name], 'state': ['Succeeded']}})
-
-    input_json = {
-        "src_bucket_name": src_bucket_name, 
-        "src_source_name": src_source_name,
-        "src_table_name": table_name,
-        "src_database_name": glue_db_name,
-        "trg_table_name": table_name,
-        "trg_database_name": db_name,
-        "trg_database_schema_name": db_schema }
 
     # Create the rule first
     rule_response = event_client.put_rule(
@@ -143,12 +134,12 @@ def create_cloudwatch_event(glue_db_name, gluejb_lambda_arn, src_bucket_name, sr
     # Place the lambda target for the rule
     response = event_client.put_targets(
         Rule=rule_name,
-        Targets=[{'Id': rule_name, 'Arn': gluejb_lambda_arn, 'Input': json.dumps(input_json)}]
+        Targets=[{'Id': rule_name, 'Arn': target_lambda_arn}, ]
     )
 
     # Grant invoke permission to Lambda
     lambda_client.add_permission(
-        FunctionName=gluejb_lambda_arn,
+        FunctionName=target_lambda_name,
         StatementId=rule_name,
         Action='lambda:InvokeFunction',
         Principal='events.amazonaws.com',
@@ -156,84 +147,62 @@ def create_cloudwatch_event(glue_db_name, gluejb_lambda_arn, src_bucket_name, sr
     )
 
 # -------------------------------------------------
-# Check if glue job exists
+# get the glue daas client
 # -------------------------------------------------
-def gluejob_exists(glue_job_name):
-    try:
-        response = glue_client.get_job(JobName=glue_job_name)
-        return True
-    except glue_client.exceptions.EntityNotFoundException as e:
-        return False
-
-
-
-
-# -------------------------------------------------
-# Create the cloudwatch event for the crawler
-# -------------------------------------------------
-def invoke_gluejob(glue_job_name, gluejb_lambda_arn, src_bucket_name, src_source_name, domain_name, glue_db_name, 
-                table_name, db_name, db_schema):
-    if gluejob_exists(glue_job_name):
-        response = glue_client.start_job_run(JobName = glue_job_name)
-    else:
-        data={}
-        data['src_bucket_name'] = src_bucket_name
-        data['src_source_name'] = src_source_name
-        data['src_table_name'] = table_name
-        data['src_database_name'] = glue_db_name
-        data['trg_table_name'] = table_name
-        data['trg_database_name'] = db_name
-        data['trg_database_schema_name'] = db_schema
-        json_str=json.dumps(data)
-        response = lambda_client.invoke(
-            FunctionName= gluejb_lambda_arn, 
-            InvocationType = "Event", 
-            Payload = json_str
-        )
-        response = response['Payload'].read()  
-    return response
-
-
-
+def get_glue_client(target_glue_service_role_arn):
+    sts_connection = boto3.client('sts')
+    # target_glue_service_role_arn = 'arn:aws:iam::708439727567:role/rle-ganesh-lmd-metadata-generator-poc'
+    print('assuming role..... ' + target_glue_service_role_arn)
+    
+    daas_client = sts_connection.assume_role(
+        RoleArn=target_glue_service_role_arn,
+        RoleSessionName="daas-core"
+    )
+    ACCESS_KEY = daas_client['Credentials']['AccessKeyId']
+    SECRET_KEY = daas_client['Credentials']['SecretAccessKey']
+    SESSION_TOKEN = daas_client['Credentials']['SessionToken']
+    glue_client = boto3.client('glue', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY, aws_session_token=SESSION_TOKEN)
+    print('got the glue_client !!!!!!!@')
+    return glue_client
+    
+    
 # -------------------------------------------------
 # Main lambda function
 # -------------------------------------------------
 def lambda_handler(event, context):
-    init()
     print(event)
     try:
-        glue_db_name = event['glue_db_name']
-        lakeformation_role_name = event['lakeformation_role_name']
-        gluejb_lambda_arn = event['gluejb_lambda_arn']
-        src_bucket_name = event['src_bucket_name']
-        src_source_name = event['src_source_name']
-        crawler_name = event['crawler_name']
-        source_file_path = event['source_file_path']
-        domain_name = event['domain_name']
-        table_name = event['table_name']
-        partitions = event['partitions']
-        partition_values=event['partition_values']
-        replicate = event['replicate']
-        db_name = event['db_name']
-        db_schema = event['db_schema']
-        if crawler_exits(crawler_name):
-            add_table_partitions(glue_db_name, table_name, partitions, partition_values)
-            if replicate:
-                glue_job_name = entity + '_glujb_' + table_name + '_' + environment
-                invoke_gluejob(glue_job_name, gluejb_lambda_arn, src_bucket_name, src_source_name, domain_name, glue_db_name, 
-                            table_name, db_name, db_schema)
+        account_id = event['account_id']
+        params = json.loads(event['params'])
+        glue_db_name = params['glue_db_name']
+        print(glue_db_name)
+        glue_admin_role_name = params['glue_admin_role_name']
+        target_lambda_name = params['target_lambda_name']
+        target_lambda_arn = params['target_lambda_arn']
+        crawler_name = params['crawler_name']
+        source_file_path = params['source_file_path']
+        domain_name = params['domain_name']
+        table_name = params['table_name']
+        partitions = params['partitions']
+        partition_values=params['partition_values']
+        target_glue_service_role_arn='arn:aws:iam::' + account_id + ':role/' + glue_admin_role_name
+        print(target_glue_service_role_arn)
+        glue_client = get_glue_client(target_glue_service_role_arn)
+        if crawler_exits(glue_client, crawler_name):
+            print('1a')
+            add_table_partitions(glue_client, glue_db_name, table_name, partitions, partition_values)
         else:
-            create_crawler(glue_db_name, lakeformation_role_name, crawler_name, domain_name, source_file_path)
-            start_crawler(crawler_name)
-            create_cloudwatch_event(glue_db_name, gluejb_lambda_arn, src_bucket_name, src_source_name, domain_name, table_name, 
-                        db_name, db_schema, crawler_name)
+            print('1b')
+            create_crawler(glue_client, glue_db_name, glue_admin_role_name, crawler_name, domain_name, source_file_path)
+            print('1c')
+            start_crawler(glue_client, crawler_name)
+            # create_cloudwatch_event(glue_client, crawler_name, target_lambda_arn, target_lambda_name)
         return {
             'body': json.dumps('SUCCESS'),
             'statusCode': 200
         }
     except Exception as e:
-        print(e)
         return {
-            'body': e,
+            'body': json.loads(json.dumps(e, default=str)),
             'statusCode': 400
         }
