d3a3c7d14f55130d95f982de1032f45d1ecd8a75
Ganesh Nathan <nathan.ganesh@gmail.com>
2022-06-14 20:14:31 -0700

    updating

--- daas-core/deploy-daas-core.sh
+++ daas-core/deploy-daas-core.sh
@@ -35,21 +35,24 @@ then
     # deploy_stack tag
     # deploy_stack vpc
     # sleep 90
-    deploy_stack ngw
+    
+    # deploy_stack ngw
     # deploy_stack sqs ingest-daas-core 
     # deploy_stack sgrp lmd-default
     # deploy_stack lmdlyr xmltodict
+    # sleep 90
+
     # deploy_stack lmd ingest-invoker 1
     # deploy_stack lmd raw-metadata-generator 1  
     # deploy_stack lmd raw-metadata-purger 1  
-    # deploy_stack lmd excel-processor 1
+    deploy_stack lmd excel-processor 1
     # deploy_stack lmd xml-processor 1
     # deploy_stack stpfn event-converter
     # deploy_stack stpfn event-controller
-    # deploy_stack sqs curate-daas-core 
-    # deploy_stack lmd curate-model-generator 1
     # deploy_stack s3 daas-core-setup-bucket
 
+    # deploy_stack sqs curate-daas-core 
+    # deploy_stack lmd curate-model-generator 1
 else
     echo "Missing required parameter. Usage: deploy-stack.sh <entity> <account id> <region> <environment>"
 fi
